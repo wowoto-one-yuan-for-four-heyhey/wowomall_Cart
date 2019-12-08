48329a7458ad72fd80719cb159e50f72ad526096
medalwill <42199368+medalwill@users.noreply.github.com>
2019-12-08 12:00:23 +0800

    1
    1

---
+++ src/test/java/com/xmu/wowomall/cart/CartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xmu.wowomall.cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
