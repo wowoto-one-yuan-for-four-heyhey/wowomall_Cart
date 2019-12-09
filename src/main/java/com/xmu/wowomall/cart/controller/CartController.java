a5842a23502e74cf65ba9ce64d962864e0e01011
muluoyanran <muluoyanran@users.noreply.github.com>
2019-12-09 18:44:45 +0800

    原始文件

--- src/main/java/com/xmu/wowomall/cart/controller/CartController.java
+++ src/main/java/com/xmu/wowomall/cart/controller/CartController.java
@@ -28,6 +28,8 @@ public class CartController {
      * @param userId   用户ID
      * @param page     分页页数
      * @param limit     分页大小
+     * @param sort      以什么为序
+     * @param order     升/降序
      * @return 订单列表
      */
     @GetMapping("carts")
@@ -35,12 +37,13 @@ public class CartController {
     public Object getCarts(Integer userId,
                             @ApiParam(name="page",value="页码",required=true) @RequestParam(defaultValue = "1")Integer page,
                             @ApiParam(name="limit",value="每页条数",required=true) @RequestParam(defaultValue = "10")Integer limit,
-                            @ApiParam(name="sort",value="以什么为序",required=true) @RequestParam(defaultValue = "add_time") String sort)
+                            @ApiParam(name="sort",value="以什么为序",required=true) @RequestParam(defaultValue = "add_time") String sort,
+                            @ApiParam(name="order",value="升/降序",required=true) @RequestParam(defaultValue = "desc") String order)
     {
         if(null != userId) {
             return ResponseUtil.unlogin();
         }
-        return ResponseUtil.fail();
+        return cartService.getCarts(userId,page, limit, sort, order);
     }
 
 
