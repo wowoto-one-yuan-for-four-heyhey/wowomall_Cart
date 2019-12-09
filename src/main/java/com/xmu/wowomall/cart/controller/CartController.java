41da7e2815dfd7c8cbb724aed260ac0b1a0a3494
muluoyanran <muluoyanran@users.noreply.github.com>
2019-12-09 17:45:58 +0800

    原始文件

--- src/main/java/com/xmu/wowomall/cart/controller/CartController.java
+++ src/main/java/com/xmu/wowomall/cart/controller/CartController.java
@@ -1,6 +1,51 @@
 package com.xmu.wowomall.cart.controller;
 
+
+import com.xmu.wowomall.cart.service.CartService;
+import com.xmu.wowomall.cart.util.ResponseUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(value="carts",tags = "购物车")
+@RestController
 public class CartController {
+
+    private static final Logger logger = LoggerFactory.getLogger(CartController.class);
+
+    @Autowired
+    private CartService cartService;
+
+    /**
+     * 获取用户订单列表
+     *
+     * @param userId   用户ID
+     * @param page     分页页数
+     * @param limit     分页大小
+     * @return 订单列表
+     */
+    @GetMapping("carts")
+    @ApiOperation(value = "用户获取购物车列表/list", notes = "用户获取订单列表")
+    public Object getCarts(Integer userId,
+                            @ApiParam(name="page",value="页码",required=true) @RequestParam(defaultValue = "1")Integer page,
+                            @ApiParam(name="limit",value="每页条数",required=true) @RequestParam(defaultValue = "10")Integer limit,
+                            @ApiParam(name="sort",value="以什么为序",required=true) @RequestParam(defaultValue = "add_time") String sort)
+    {
+        if(null != userId) {
+            return ResponseUtil.unlogin();
+        }
+        return ResponseUtil.fail();
+    }
+
+
+
+
 
 
 }
