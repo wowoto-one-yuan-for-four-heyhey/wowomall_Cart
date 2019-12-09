17aa8530ab30ce104fc0c28e49179fd23200b824
muluoyanran <muluoyanran@users.noreply.github.com>
2019-12-09 20:01:40 +0800

    原始文件

--- src/main/java/com/xmu/wowomall/cart/controller/CartController.java
+++ src/main/java/com/xmu/wowomall/cart/controller/CartController.java
@@ -66,6 +66,63 @@ public class CartController {
         return null;
     }
 
+    /**
+     * 立即购买
+     * <p>
+     * 和add方法的区别在于：
+     * 1. 如果购物车内已经存在购物车货品，前者的逻辑是数量添加，这里的逻辑是数量覆盖
+     * 2. 添加成功以后，前者的逻辑是返回当前购物车商品数量，这里的逻辑是返回对应购物车项的ID
+     *
+     * @param userId 用户ID
+     * @param cart   购物车商品信息， { goodsId: xxx, productId: xxx, number: xxx }
+     * @return 立即购买操作结果
+     */
+    @PostMapping("carts/{id}")
+    @ApiOperation(value = "添加一个购物车明细，如果已经存在这种商品，就进行数量覆盖，否则就新增购物车明细 /fastadd")
+    public Object fastadd( Integer userId, @RequestBody String cart){
+        return null;
+    }
+
+    /**
+     * 修改购物车商品货品数量
+     *
+     * @param userId 用户ID
+     * @param cart   购物车商品信息， { id: xxx, goodsId: xxx, productId: xxx, number: xxx }
+     * @return 修改结果
+     */
+    @PutMapping("carts/{id}")
+    @ApiOperation(value = "修改某个购物车项信息 /update")
+    public Object update(Integer userId, @RequestBody String cart){
+        return null;
+    }
+
+    /**
+     * 购物车商品删除
+     *
+     * @param userId 用户ID
+     * @param body   购物车商品信息， { productIds: xxx }
+     * @return 购物车信息
+     * 成功则
+     * {
+     * errno: 0,
+     * errmsg: '成功',
+     * data: xxx
+     * }
+     * 失败则 { errno: XXX, errmsg: XXX }
+     */
+    @DeleteMapping("carts/{id}")
+    @ApiOperation(value = "删除一个购物车项 /delete")
+    public Object delete(Integer userId, @RequestBody String body){
+        return null;
+    }
+
+
+
+
+
+
+
+
 
 
 
