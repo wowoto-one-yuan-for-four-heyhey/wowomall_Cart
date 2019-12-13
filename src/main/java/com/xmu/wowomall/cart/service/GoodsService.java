480e71005282e4ffd0c41ecc63123d96a9751585
Tens <tens444@163.com>
2019-12-13 20:18:16 +0800

    cloud

--- src/main/java/com/xmu/wowomall/cart/service/GoodsService.java
+++ src/main/java/com/xmu/wowomall/cart/service/GoodsService.java
@@ -4,6 +4,7 @@ import com.xmu.wowomall.cart.domain.Product;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 /**
  * @Author: Tens
@@ -11,8 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
  * @Date: 2019/12/12 22:56
  */
 @Service
-@FeignClient("wowoto-goods")
+@FeignClient("goods")
 public interface GoodsService {
     @GetMapping("product/{id}")
-    Product getProductById(Integer productId);
+    Product getProductById(@PathVariable("id") Integer productId);
 }
