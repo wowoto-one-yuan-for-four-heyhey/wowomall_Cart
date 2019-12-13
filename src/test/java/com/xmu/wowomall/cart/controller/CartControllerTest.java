9ba3d57bd17c41258265d8d997812481f46af2dd
Tens <tens444@163.com>
2019-12-13 20:15:59 +0800

    cloud

---
+++ src/test/java/com/xmu/wowomall/cart/controller/CartControllerTest.java
@@ -0,0 +1,21 @@
+package com.xmu.wowomall.cart.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @Author: Tens
+ * @Description:
+ * @Date: 2019/12/13 20:07
+ */
+public class CartControllerTest {
+
+    @Autowired
+    CartController cartController;
+
+    @Test
+    public void findCartItemById(){
+        cartController.findCartItemById(1);
+    }
+
+}
